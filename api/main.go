7c0fe396a1642e603c928e28c801554f5c21b823
johnsouza <j.gabrieltec@gmail.com>
2026-01-07 00:48:45 -0300

    chore: update dependencies and Go version; add REST API documentation

--- api/main.go
+++ api/main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	envconfig "github.com/caarlos0/env/v10"
 	"github.com/gabrielsouzacoder/clean-new/api/routes"
 	"github.com/gabrielsouzacoder/clean-new/infrastructure/repository"
 	"github.com/gabrielsouzacoder/clean-new/usecase/todo"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"os"
 )
+
+type Config struct {
+	Port     string `env:"PORT" envDefault:"8080"`
+	DBType   string `env:"DB_TYPE" envDefault:"inmemory"`
+	MongoURI string `env:"MONGO_URI" envDefault:"mongodb://localhost:27017"`
+}
 
 func main() {
 	fmt.Println("[Server] Initializing ...")
-	loadEnvironment()
-	todoRepo := selectDatabase()
+	config := loadEnvironment()
+	todoRepo := selectDatabase(config)
 	todoService := todo.NewService(todoRepo)
 
-	server := NewServer()
+	server := NewServer(config)
 	server.Run(todoService)
 }
 
-func loadEnvironment() {
+func loadEnvironment() Config {
 	err := godotenv.Load(".env")
 
 	if err != nil {
 		log.Println("[Warning] The .env file could not be loaded")
 	}
+
+	var config Config
+	if err := envconfig.Parse(&config); err != nil {
+		log.Fatal(err)
+	}
+
+	return config
 }
 
 type Server struct {
@@ -35,9 +49,9 @@ type Server struct {
 	server *gin.Engine
 }
 
-func NewServer() Server {
+func NewServer(config Config) Server {
 	return Server{
-		port:   "8080",
+		port:   config.Port,
 		server: gin.Default(),
 	}
 }
@@ -49,13 +63,11 @@ func (s *Server) Run(todo *todo.Service) {
 	log.Fatal(router.Run(":" + s.port))
 }
 
-func selectDatabase() todo.Repository {
-	dbType := os.Getenv("DB_TYPE")
-
+func selectDatabase(config Config) todo.Repository {
 	var todoRepo todo.Repository
 
-	if dbType == "mongo" {
-		todoRepo = repository.NewMongoDbRepository(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if config.DBType == "mongo" {
+		todoRepo = repository.NewMongoDbRepository(options.Client().ApplyURI(config.MongoURI))
 	} else {
 		todoRepo = repository.NewInMemoryDatabase()
 	}
